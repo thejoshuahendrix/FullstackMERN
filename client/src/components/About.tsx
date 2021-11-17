8907d4fe5beb638dc533da4f8fb472a8a17fb7ad
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-11-17 11:03:45 -0500

    fixed about me margin

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -8,7 +8,7 @@ const About = ({ isLoggedIn }: PropTypes) => {
 
         <div>
             <Hero isLoggedIn={isLoggedIn} header="About Page" />
-            <div style={{ margin: "30%", display: "flex", flexDirection: 'column' }}>This project was created as a part of my portfolio.
+            <div style={{ margin: "30%", marginTop: "2rem", display: "flex", flexDirection: 'column' }}>This project was created as a part of my portfolio.
                 <p>I am Joshua Hendrix, full-stack developer.
                     I love to create and engineer full-stack applications.
                     I appreciate you taking the time to read this about me.</p>
